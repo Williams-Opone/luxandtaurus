736d91d29d9a50cddbdcc4708bceaf480c361016
fendyboy <oponeboboola@gmail.com>
2026-03-13 14:48:36 +0100

    Working

---
+++ starter.py
@@ -0,0 +1,7 @@
+from project import app,db
+
+if __name__=='__main__':
+    with app.app_context():
+        db.create_all()  # This creates the tables if they don't exist
+    app.run(debug=True,port=7070)
+    

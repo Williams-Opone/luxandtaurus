a1479bac5498e3a956890648a7bcaa82f8585c01
fendyboy <oponeboboola@gmail.com>
2026-03-14 05:14:44 +0100

    Chore: Add Vercel configuration

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from project import app,db
 if __name__=='__main__':
     with app.app_context():
         db.create_all()  # This creates the tables if they don't exist
-    app.run(debug=True,port=7070)
+    
     

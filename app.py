e4e6774f908b9d82cc13022cb40d21a5545b7f9e
fendyboy <oponeboboola@gmail.com>
2026-03-14 09:42:03 +0100

    fixed circular import

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 import sys
 import os
 
-# This forces Vercel to see the 'project' folder sitting right next to app.py
-sys.path.append(os.path.join(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-try:
-    from project import app, db
-except ImportError:
-    # This is a backup in case Vercel's working directory is different
-    sys.path.append(os.getcwd())
-    from project import app, db
+from project import app
 
 if __name__ == "__main__":
     app.run()

75235ce51cbfe20782afa89e23706be646431752
fendyboy <oponeboboola@gmail.com>
2026-03-14 09:46:23 +0100

    fixed circular import2

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+# Force-add the directory containing app.py to sys.path
+root_dir = os.path.dirname(os.path.abspath(__file__))
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
 
 from project import app
 
